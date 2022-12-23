f2b2cdb7f8a64348082c30ae74a00d157bee1c4f
Cristian S <christian.bx100pre@gmail.com>
2022-12-23 13:58:59 -0500

    se agrego el modulo admin (loguin, dashboard), se hizo responsive este mismo

--- src/app/auth/admin-auth/assets/js/animate-login.js
+++ src/app/auth/admin-auth/assets/js/animate-login.js
@@ -1,19 +1,19 @@
 var working = false;
 $('.login').on('submit', function(e) {
   e.preventDefault();
-  if (working) return;
-  working = true;
+  // if (working) return;
+  // working = true;
   var $this = $(this),
     $state = $this.find('button > .state');
   $this.addClass('loading');
   $state.html('Autenticando...');
-  setTimeout(function() {
-    $this.addClass('ok');
-    $state.html('¡Bienvenido de nuevo!');
-    setTimeout(function() {
-      $state.html('Log in');
-      $this.removeClass('ok loading');
-      working = false;
-    }, 3000);
-  }, 3000);
+  // setTimeout(function() {
+  //   $this.addClass('ok');
+  //   //$state.html('¡Bienvenido de nuevo!');
+  //   setTimeout(function() {
+  //     $state.html('Iniciar sesión');
+  //     $this.removeClass('ok loading');
+  //     working = false;
+  //   }, 1000);
+  // }, 1000);
 });
